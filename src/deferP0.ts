33df41b4a203e978df73157b709f6594837e3d7b
Chris Sperry <christophermartinsperry@gmail.com>
2019-04-06 04:09:52 +0100

    fix(deferP0): change deferred parameter name from a to src

--- src/deferP0.ts
+++ src/deferP0.ts
@@ -1,3 +1,3 @@
-export const deferP0 = <P0, A extends any[], R>(fn: (a: P0, ...args: A) => R) => (...args: A) => (
-  a: P0
-): R => fn(a, ...args)
+export const deferP0 = <P0, A extends any[], R>(fn: (src: P0, ...args: A) => R) => (...args: A) => (
+  src: P0
+): R => fn(src, ...args)
