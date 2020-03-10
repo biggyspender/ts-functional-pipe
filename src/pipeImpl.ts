f6d582dba3701359747f8db7ecc1a1e807ab3202
Chris Sperry <chris.sperry@outthink.io>
2020-03-10 20:06:00 +0000

    fix(pipe/compose): generate correct implementations of pipe and compose
    run codegen to create versions of pipe and compose that differ from previous versions
    
    BREAKING CHANGE: semantics of pipe and compose have changed. See README

---
+++ src/pipeImpl.ts
@@ -0,0 +1,9 @@
+import { Func } from './types/Func'
+import { UnaryFunction } from './types/UnaryFunction'
+
+export function pipeImpl<T extends any[], R>(
+  ...args: [o1: Func<T, any>, ...operations: UnaryFunction<any, any>[]]
+): Func<T, R> {
+  const [o1, ...operations] = args
+  return (...args: T) => operations.reduce((acc, f) => f(acc), o1(...args))
+}
