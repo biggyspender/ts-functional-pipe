79a453b0ef7daedfbda5142f7a34acee35a9febe
Chris Sperry <chris.sperry@outthink.io>
2020-03-10 20:01:00 +0000

    build(update deps): update all NPM dependencies
    Update all NPM deps and change layout of distributed package. Turn on TS importHelpers option and
    treat as external in rollup config.
    
    BREAKING CHANGE: lib folder is no longer included in published package

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,26 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
 
 const libraryName = 'ts-functional-pipe'
 
+const externals = ["tslib",]
+
+const umdGlobals = externals => externals.map(moduleName => [moduleName, camelCase(moduleName)]).reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
+
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true, globals: umdGlobals(externals) },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: externals,
   watch: {
     include: 'src/**',
   },
