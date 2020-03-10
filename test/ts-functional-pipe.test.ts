1ffa778d999d5d7eaf8bdbdce863675fc343d7aa
Chris Sperry <chris.sperry@outthink.io>
2020-03-10 20:04:00 +0000

    feat(composition): allow variadic function params for 1st func in composition chain
    BREAKING CHANGE: consumers of functions pipe, compose, typedCompose and typedPipe will to update
    generic parameter from (for instance) typedCompose<string,...> to typedCompose<[string],...> where
    the 1st generic parameter is now a tuple describing variadic parameters of first function in
    composition chain rather that a single unary parameter

--- test/ts-functional-pipe.test.ts
+++ test/ts-functional-pipe.test.ts
@@ -1,9 +1,16 @@
-import { pipe, typedPipe, pipeValue, pp, compose, typedCompose } from '../src/ts-functional-pipe'
+import {
+  pipe,
+  typedPipe,
+  pipeValue,
+  pp,
+  compose,
+  typedCompose,
+  deferP0,
+} from '../src/ts-functional-pipe'
 import { toIterable } from './toIterable'
-import { deferP0 } from '../src/deferP0'
 
 const _map = <T, TOut>(src: Iterable<T>, selector: (v: T, i: number) => TOut): Iterable<TOut> =>
-  toIterable(function*() {
+  toIterable(function* () {
     let c = 0
     for (const v of src) {
       yield selector(v, c++)
@@ -11,7 +18,7 @@ const _map = <T, TOut>(src: Iterable<T>, selector: (v: T, i: number) => TOut): I
   })
 
 const _filter = <T>(src: Iterable<T>, pred: (v: T, i: number) => boolean): Iterable<T> =>
-  toIterable(function*() {
+  toIterable(function* () {
     let i = 0
     for (const x of src) {
       if (pred(x, i++)) {
@@ -25,8 +32,8 @@ const filter = deferP0(_filter)
 const toArray = <T>(v: Iterable<T>) => [...v]
 
 describe('ts-functional-pipe', () => {
-  const tp = typedCompose<Iterable<number>>()
-  const p = tp(map(x => (x * 2).toString()))
+  const tp = typedCompose<[Iterable<number>]>()
+  const p = tp(map((x) => (x * 2).toString()))
   it('pipes', () => {
     expect([...p([1, 2, 3])]).toEqual(['2', '4', '6'])
 
@@ -35,17 +42,14 @@ describe('ts-functional-pipe', () => {
     const mulStrPipe = compose(map((x: number) => (x * 2).toString()))
 
     expect([...pipeValue([1, 2, 3]).into(mulStrPipe)]).toEqual(['2', '4', '6'])
-    expect([...pipeValue([1, 2, 3]).into(filter(x => x % 2 === 1))]).toEqual([1, 3])
+    expect([...pipeValue([1, 2, 3]).into(filter((x) => x % 2 === 1))]).toEqual([1, 3])
     const dinosaurify = (name: string) => `${name}-o-saurus`
     const sayHello = (name: string) => `Hello, ${name}!`
-    const sayHelloToDinosaur = compose(
-      dinosaurify,
-      sayHello
-    )
+    const sayHelloToDinosaur = compose(dinosaurify, sayHello)
     expect(sayHelloToDinosaur('mike')).toBe('Hello, mike-o-saurus!')
-    const oddMultipliedByTwo = typedCompose<Iterable<number>>()(
-      filter(x => x % 2 === 1),
-      map(x => x * 2)
+    const oddMultipliedByTwo = typedCompose<[nums: Iterable<number>]>()(
+      filter((x) => x % 2 === 1),
+      map((x) => x * 2)
     )
     const q = oddMultipliedByTwo([1, 2, 3])
     expect([...q]).toEqual([2, 6])
@@ -55,7 +59,7 @@ describe('ts-functional-pipe', () => {
 
     const composedFuncs = compose(
       filter((x: number) => x % 2 === 0),
-      map(x => '$' + x),
+      map((x) => '$' + x),
       toArray
     )
     expect(composedFuncs([1, 2, 3, 4])).toEqual(['$2', '$4'])
