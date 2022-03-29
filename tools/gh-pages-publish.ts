ba871cfc518fa24397611cd5565f588ee782d6f4
Chris Sperry <chris.sperry@outthink.io>
2022-03-29 22:16:51 +0000

    build(doc-deploy): fix token issue in doc deploy

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -26,6 +26,6 @@ exec('git config user.name "Chris Sperry"')
 exec('git config user.email "christophermartinsperry@gmail.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+  `git push --force --quiet "https://${repository}" master:gh-pages`
 )
 echo("Docs deployed!!")
