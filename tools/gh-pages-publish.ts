cefdb38f332d52ef600707228d2f57fad31132d7
Chris Sperry <chris.sperry@outthink.io>
2022-03-29 22:48:57 +0000

    ci(deploy-docs): attempt to fix doc-deploy

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -26,7 +26,8 @@ exec("git add .")
 exec('git config user.name "Chris Sperry"')
 exec('git config user.email "biggyspender@users.noreply.github.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
+echo(`git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`)
 exec(
-  `git push --force --quiet "https://biggyspender:${ghToken}@${repository}" master:gh-pages`
+  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
 )
 echo("Docs deployed!!")
