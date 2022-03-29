3d66f8564ecceca8cca4f1200ac4cc24d5cdcc41
Chris Sperry <chris.sperry@outthink.io>
2022-03-29 20:16:58 +0000

    feat(deferp0): improve parameter name inference of deferred parameter
    Improved inference of deferred parameter name after using deferP0.
    
    BREAKING CHANGE: Signature of functions returned by deferP0 is changed (improved)

--- src/deferP0.ts
+++ src/deferP0.ts
@@ -1,10 +1,19 @@
+type Tail<T extends readonly unknown[]> = T extends readonly [unknown, ...infer R] ? R : []
+
+type HeadAsTuple<T extends readonly unknown[]> = T extends [unknown, ...infer RestTup]
+    ? T extends [...infer HeadTup, ...RestTup]
+        ? HeadTup
+        : never
+    : []
+
 /**
  * Curry 1st parameter of a function
  * @param fn a function of form `(p0: P0, p1: P1, p2: P2) => R`
  * @returns a function of form `(p1: P1, p2: P2) => (p0: P0) => R`
  */
 export const deferP0 =
-    <P0, A extends any[], R>(fn: (src: P0, ...args: A) => R) =>
-    (...args: A) =>
-    (src: P0): R =>
-        fn(src, ...args)
+    <A extends readonly unknown[], R>(fn: (...args: A) => R) =>
+    (...args: Tail<A>) =>
+    (...src: HeadAsTuple<A>): R => {
+        return fn(...(src.concat(args) as unknown as A))
+    }
